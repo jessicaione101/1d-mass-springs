40fe7fc281864d93499c907812cc066415be7ce3
Jessica <jessicaione101@gmail.com>
2021-07-08 18:01:38 -0700

    Start Runge-Kutta integration

--- include/runge_kutta.h
+++ include/runge_kutta.h
@@ -7,9 +7,45 @@
 //Output:
 //  q - set q to the updated generalized coordinate using Runge-Kutta time integration
 //  qdot - set qdot to the updated generalized velocity using Runge-Kutta time integration
-
+#include <iostream>
 template<typename FORCE> 
 inline void runge_kutta(Eigen::VectorXd &q, Eigen::VectorXd &qdot, double dt, double mass,  FORCE &force) {
-
-    
+  Eigen::MatrixXd A_inv(2, 2);
+  A_inv(0, 0) = 1/mass;
+  A_inv(0, 1) = 0;
+  A_inv(1, 0) = 0;
+  A_inv(1, 1) = 1;
+  
+  Eigen::MatrixXd y(2, 1);
+  y(0, 0) = qdot(0);
+  y(1, 0) = q(0);
+  
+  Eigen::VectorXd f;
+  force(f, q, qdot);
+  
+  
+  
+  
+  
+  Eigen::MatrixXd f_y(2, 1);
+  f_y(0, 0) = f(0);
+  f_y(1, 0) = qdot(0);
+  
+  
+  
+  Eigen::MatrixXd y_next(2, 1);
+  
+  
+  
+  static bool once = true;
+  if (once) {
+    std::cout << "---" << std::endl;
+    std::cout << qdot << std::endl;
+    std::cout << "---" << std::endl;
+    std::cout << q << std::endl;
+    std::cout << "---" << std::endl;
+    std::cout << y << std::endl;
+    std::cout << "---" << std::endl;
+    once = false;
+  }
 }
